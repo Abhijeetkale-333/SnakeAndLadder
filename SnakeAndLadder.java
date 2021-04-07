2814d50c01aa9a69aa773a0dd201b1935723dea7
Your Name <you@example.com>
2021-04-07 22:03:18 +0530

    added:RollTheDice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,21 +1,19 @@
-package SnakeAndladder;
-
-
-public class SinglePlayerAtStart {
+public class RollTheDice {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Wellcome to snake ladder game: ");
+
+		int startPosition = 0;
+		int minimum = 1;
+		int maximum = 6;
+
+		int randomValue = (int)Math.floor(Math.random() * 7);
 		
-		
-		int start = 0;
-		int end = 100;
-		
-		int PlayerPosition = start;
-		
-		System.out.println("PlayerPosition is "+PlayerPosition);
-		
-		
+				
+		System.out.println("Random number between 1 to 6 = " + randomValue);
 
 	}
 
-}
+
+	}

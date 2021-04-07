e116c703a7aefe0f55059b9f43bd0a3e2118e314
Your Name <you@example.com>
2021-04-07 22:44:28 +0530

    Added:WinTheGame

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,38 +1,42 @@
-public class ExactWinningPosition {
+public class WinTheGame {
 	public static void main(String[] args) {
-	 	System.out.println("Welcome to snake ladder game: ");
-	
-	int position = 0;
-	int TempPosition;
-	int minimum = 1;
-	int maximum = 6;
+	//System.out.println("Welcome to snake ladder game: ");
 
-	while(position < 100)
-	{
-		int randomValue = (int) Math.floor(Math.random() * (maximum - minimum + 1) + minimum);  //Math.random()*(max-min+1)+min
-//		System.out.println("randomvalue" + randomValue);
+			int position = 0;
+		//	int TempPosition;
+			int minimum = 1;
+			int maximum = 6;
+			int countDice = 0;
 
-		int randomOption = (int) (Math.random() * 10) % 3;
-//		System.out.println("randomoption" + randomOption);
+			while(position < 100)
+			{
+				int randomValue = (int) Math.floor(Math.random() * (maximum - minimum + 1) + minimum);  //Math.random()*(max-min+1)+min
+//				System.out.println("randomvalue" + randomValue);
 
-		if(randomOption == 0 && position == 0)
-			position = 0;
+				int randomOption = (int) (Math.random() * 10) % 3;
+//				System.out.println("randomoption" + randomOption);
 
-		else if(randomOption == 1 && position < 100)
-		{
-			int storeValue = position;
-			position += randomValue;
-			if(position > 100)
-				position = storeValue;
+				if(randomOption == 0 && position == 0)
+					position = 0;
+
+				else if(randomOption == 1 && position < 100)
+				{
+					int storeValue = position;
+					position += randomValue;
+					if(position > 100)
+						position = storeValue;
+				}
+				else if(randomOption ==2)
+				{
+					position -= randomValue;
+					if(position < 0 )
+						position = 0;
+				}
+				countDice++;
+				System.out.println("position" + position);
+			}
+			System.out.println("Number of times dice was played to win the game " + countDice);
 		}
-		else if(randomOption ==2)
-		{
-			position -= randomValue;
-			if(position < 0 )
-				position = 0;
-		}
-	  System.out.println("position" + position);
-	
+
+
 	}
-	}
-}

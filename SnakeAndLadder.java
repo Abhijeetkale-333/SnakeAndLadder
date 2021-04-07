ec90bd25fd8a46fa61b405f2ed48b66d1d05727d
Your Name <you@example.com>
2021-04-07 21:59:55 +0530

    added:SinglePlayerAtStart

---
+++ SnakeAndLadder.java
@@ -0,0 +1,21 @@
+package SnakeAndladder;
+
+
+public class SinglePlayerAtStart {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int start = 0;
+		int end = 100;
+		
+		int PlayerPosition = start;
+		
+		System.out.println("PlayerPosition is "+PlayerPosition);
+		
+		
+
+	}
+
+}

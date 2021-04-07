20044eaf979bbc6335f236bcdc30421acc1d4591
Your Name <you@example.com>
2021-04-07 21:49:02 +0530

    added:RollTheDice

---
+++ snakeLadder.java
@@ -0,0 +1,19 @@
+public class RollTheDice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Wellcome to snake ladder game: ");
+
+		int startPosition = 0;
+		int minimum = 1;
+		int maximum = 6;
+
+		int randomValue = (int)Math.floor(Math.random() * 7);
+		
+				
+		System.out.println("Random number between 1 to 6 = " + randomValue);
+
+	}
+
+
+	}
